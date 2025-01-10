4164d17e0bebdef76a4d81ab924b9771985a7605
DavidMesic <142584788+DavidMesic@users.noreply.github.com>
2025-01-10 16:02:14 +0100

    Init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SaveUpApp.ViewModels;
+namespace SaveUpApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+    }
+
+}
